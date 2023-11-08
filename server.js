f059541fa4cb840e4db42c0a3465b78fac660e65
prinako <nyankonton122@gmail.com>
2023-11-08 20:09:40 -0300

    sever slow

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ const {
 } = require("./database/querys");
 
 const port = process.env.PORT;
+
+const app = express();
+
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = mongoose.connect(process.env.MONGODB,{dbName:process.env.DBname});
+    const conn = await mongoose.connect(process.env.MONGODB, {
+      dbName: process.env.DBname,
+    });
     console.log(`MongoDB Connected: ${conn.connection}`);
   } catch (err) {
     console.log(err);
@@ -22,18 +27,21 @@ const connectDB = async () => {
   }
 };
 
-const app = express();
+
+connectDB().then(() => {
+ 
+});
 
 let options = {
   dotfiles: "ignore",
   etag: false,
-  extensions: ["htm", "html", "css", "js", "ico", "jpg", "jpeg", "png", "svg"],
-//   index: ["index.html"],
+  extensions: ["css", "js", "ico", "jpg", "jpeg", "png", "svg"],
+    index: ["index.html"],
   maxAge: "1m",
   redirect: false,
 };
 
-app.use('/public', express.static(__dirname+'/public'));
+app.use("/public", express.static(__dirname + "/public",options));
 
 // app.use(cors());
 app.use(express.json());
@@ -42,23 +50,25 @@ app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
   const count = await getAllAcess((doc) => doc);
-    if(count.lengt === 0){
-        const index = 1
-        await postNumberOfAcess(index);
-    }else{
-        const index = count.length + 1;
-        await postNumberOfAcess(index);
-    }
+  if (count.lengt === 0) {
+    const index = 1;
+    await postNumberOfAcess(index);
+  } else {
+    console.log(count);
+    const index = count.length + 1;
+    await postNumberOfAcess(index);
+  }
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.post("/", async (req, res) => {
-    await postUser(req)
-    res.sendFile(path.join(__dirname, "/tanx.html"));
+  await postUser(req);
+  res.sendFile(path.join(__dirname, "/tanx.html"));
 });
 
-connectDB().then(() => {
-  app.listen(port, () => {
+// connectDB().then(() => {
+ 
+// });
+ app.listen(port, () => {
     console.log(`server is runing on port: ${port}`);
   });
-});
